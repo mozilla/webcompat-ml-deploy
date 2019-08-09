975a9044d28f8ceb6d632009e192f590216f04ce
John Giannelos <jgiannelos@mozilla.com>
2019-08-09 10:50:44 +0200

    Add basic webhook handler

---
+++ handler.py
@@ -0,0 +1,16 @@
+import json
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def webhook(event, context):
+    """Handler for GitHub webhook"""
+
+    try:
+        # Parse data from GH event
+        hookdata = json.loads(event["body"])
+    except json.JSONDecodeError:
+        logger.error("Failed to decode JSON")
+        return {"body": json.dumps({"error": "JSON decode failure"}), "statusCode": 500}
