22c2aef4f4ab60395bce3a6f3bf02ec1ce46df98
John Giannelos <jgiannelos@mozilla.com>
2019-08-13 13:59:23 +0200

    Submit classification jobs from lambda webhook handler

--- lambda/handler.py
+++ lambda/handler.py
@@ -1,8 +1,19 @@
+from io import StringIO
+
 import json
 import logging
+import os
+
+import boto3
+import botocore.vendored.requests as requests
 
 
 logger = logging.getLogger(__name__)
+
+
+JOB_NAME = "webcompat_ml_classify"
+JOB_DEFINITION = os.environ.get("JOB_DEFINITION")
+JOB_QUEUE = os.environ.get("JOB_QUEUE")
 
 
 def webhook(event, context):
@@ -15,6 +26,17 @@ def webhook(event, context):
         logger.error("Failed to decode JSON")
         return {"body": json.dumps({"error": "JSON decode failure"}), "statusCode": 500}
 
+    io = StringIO()
+    json.dump(hookdata, io)
 
-def invalid(event, context):
-    pass
+    # Upload tmp csv file
+    response = requests.post("https://transfer.sh", data=io)
+    parameters = {"file_url": response.content}
+
+    batch = boto3.client(service_name="batch")
+    batch.submit_job(
+        jobName=JOB_NAME,
+        jobQueue=JOB_QUEUE,
+        jobDefinition=JOB_DEFINITION,
+        parameters=parameters,
+    )
