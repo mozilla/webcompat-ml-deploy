420d601f2c35f9ad475de3a4ad463a651eff48a6
John Giannelos <jgiannelos@mozilla.com>
2019-08-15 17:32:23 +0200

    Get GH issue using API and prepare CSV for model.

--- docker/run.py
+++ docker/run.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import shutil
 import subprocess
@@ -7,19 +8,23 @@ import urllib.request
 
 import boto3
 
+from webcompat_ml.utils import prepare_gh_event_invalid
+
 
 S3_RESULTS_INVALID_BUCKET = os.environ.get("S3_RESULTS_INVALID_BUCKET")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run webcompat ML task.")
-    parser.add_argument("--issue", action="store", dest="issue")
-    parser.add_argument("--file_url", action="store", dest="file_url")
+    parser.add_argument("--issue-url", action="store", dest="issue_url")
     args = parser.parse_args()
 
-    with urllib.request.urlopen(args.file_url) as response:
+    with urllib.request.urlopen(args.issue_url) as response:
         with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-            shutil.copyfileobj(response, tmp_file)
+            data = json.load(response)
+            df = prepare_gh_event_invalid(data)
+            df.to_csv(tmp_file)
+
             command = [
                 "webcompat-ml-invalid",
                 "-m",
